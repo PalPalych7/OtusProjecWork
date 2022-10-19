f25a90fccf180032c5551566e0f2f76217456367
PalPalych7 <PalPalych@bk.ru>
2022-10-19 22:02:39 +0300

    1

--- cmd/mainService/main.go
+++ cmd/mainService/main.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("loko is chempion forever!!!  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+	fmt.Println("loko is chempion forever!!!  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
 }
